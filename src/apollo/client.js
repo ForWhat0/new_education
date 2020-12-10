23b8dc40c6c8e27ba2eea139c0f8dffd134c86f8
Roman Zozulia <7923-7749-1@plus4u.net>
2020-12-10 05:06:48 +0200

    deploy

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 
 const client = new ApolloClient({
-	uri:'http://testkp.flexreality.pro/graphql',
+	uri:'https://testkp.flexreality.pro/graphql',
 	cache: new InMemoryCache()
 })
 
