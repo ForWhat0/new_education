f9e99dc50bee848c952e37a945e3896f02f6ae25
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-18 16:23:37 +0200

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const path = require("path");
 module.exports = {
     i18n: {
         locales: ['UK', 'EN','RU'],
         defaultLocale: 'UK'
     },
-}
+    trailingSlash: true,
+    webpackDevMiddleware: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+
+        return config;
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
+};
+
