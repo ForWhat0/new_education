8e47d00a8ecb44209874838cc1b5fc12f6b6fb35
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-22 13:28:58 +0200

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,6 @@ function MyApp({ Component, pageProps }) {
                           <script async type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/easing/EasePack.min.js"></script>
                           <script async type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenLite.min.js"></script>
                           <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineLite.min.js"></script>
-                          <script async type="text/javascript" src="../styles/loadFontAwesomeFonts.js"></script>
                       </Head>
                       <Component {...pageProps} />
                   </Provider>
