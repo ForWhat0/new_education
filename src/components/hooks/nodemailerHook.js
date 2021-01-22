19eee59ce987aa76f236fb13bbf18fade4a62527
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-22 14:57:55 +0200

    deploy

--- src/components/hooks/nodemailerHook.js
+++ src/components/hooks/nodemailerHook.js
@@ -2,17 +2,15 @@ const nodemailer = require("nodemailer")
 
 export const nodemailerHook = ( {subject, text,res} ) => {
 
-    const transporter = nodemailer.createTransport(smtpTransport({
-        name: serverName,
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
         host: "smtp.gmail.com",
         port: 465,
-        secure: true,
         auth: {
             user: process.env.FROM_USER_LOGIN,
             pass: process.env.FROM_USER_PASSWORD,
         }
-    }));
-
+    });
     const mailOption = {
         from: process.env.FROM_USER_LOGIN,
         to: process.env.TO_USER_LOGIN,
