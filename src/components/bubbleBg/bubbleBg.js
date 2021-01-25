3855f14c8f043f4b4b9d51071df317b9daad2051
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-25 03:29:21 +0200

    deploy

--- src/components/bubbleBg/bubbleBg.js
+++ src/components/bubbleBg/bubbleBg.js
@@ -3,20 +3,17 @@ import {useEffect} from "react";
 
 export const BubbleBg = ()=>{
     useEffect(()=>{
-         animationButton()
+        animationButton()
     },[])
     return(
         <div id="wrapper">
 
             <div id="parallax-lvl-3">
-                <div id="b0-1" className="bubble size3 blue">&nbsp;</div>
-                <div id="b0-2" className="bubble size2 blue">&nbsp;</div>
-                <div id="b0-3" className="bubble size1 blue">&nbsp;</div>
-                <div id="b0-4" className="bubble size4 yellow">&nbsp;</div>
-                <div id="b0-5" className="bubble size3 blue">&nbsp;</div>
-                <div id="b0-6" className="bubble size2 blue">&nbsp;</div>
-                <div id="b0-7" className="bubble size2 yellow">&nbsp;</div>
-                <div id="b0-8" className="bubble size1 yellow">&nbsp;</div>
+                <div id="b3-1" className="bubble size2 green">&nbsp;</div>
+                <div id="b3-2" className="bubble size2 pink">&nbsp;</div>
+                <div id="b3-3" className="bubble size1 green">&nbsp;</div>
+                <div id="b3-5" className="bubble size1 purple">&nbsp;</div>
+                <div id="b3-6" className="bubble size2 blue">&nbsp;</div>
             </div>
 
         </div>
